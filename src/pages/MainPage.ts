03c8763e5876f41dab27b072429fad2c699250dd
tonyparkerov <antonproskurov@gmail.com>
2025-02-21 10:43:01 +0200

    upd

--- src/pages/MainPage.ts
+++ src/pages/MainPage.ts
@@ -1,10 +1,9 @@
-import { Locator } from "@playwright/test";
-import Header from "../components/Header";
-import InventoryItem from "../components/InventoryItem";
 import BasePage from "./BasePage";
 
 export default class MainPage extends BasePage {
-    private inventoryName = this.page.locator('.inventory_item_name');
+    private itemName = this.page.locator('.inventory_item_name');
+    private itemDescription = this.page.locator('.inventory_item_desc');
+    private itemPrice = this.page.locator('[data-test="inventory-item-price"]');
 
     async openMainPage() {
         await this.page.goto('/inventory.html');
@@ -18,14 +17,15 @@ export default class MainPage extends BasePage {
         await this.findItemLocator(itemName).click();
     }
 
-    async parseInventoryNames(index: number) {
-        console.log('1. in method');
-        const locator = this.inventoryName.nth(index);
-        console.log(`2. in method ${locator}`);
-        const itemName = await locator.textContent();
-        console.log(`3. in method ${itemName}`);
-        if(itemName) {
-            return locator.getByText(itemName);
-        }
+    async getItemName(index: number) {
+        return await this.itemName.nth(index).textContent();
+    }
+
+    async getItemDescription(index: number) {
+        return await this.itemDescription.nth(index).textContent();
+    }
+
+    async getItemPrice(index: number) {
+        return await this.itemPrice.nth(index).textContent();
     }
 }

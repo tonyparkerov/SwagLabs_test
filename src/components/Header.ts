ea66698d2c7cc638610d16173f577135b834d818
tonyparkerov <antonproskurov@gmail.com>
2025-03-02 14:44:15 +0200

    commit

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -2,7 +2,6 @@ import BaseComponent from "./BaseComponent";
 import BmSidebar from "./BmSidebar";
 
 export default class Header extends BaseComponent {
-    private headerLabel = this.page.locator('.app_logo');
     private burgerMenuButton = this.page.locator('button#react-burger-menu-btn');
     public burgerMenuSidebar = new BmSidebar(this.page);
     private shoppingCart = this.page.locator('[data-test="shopping-cart-link"]');
